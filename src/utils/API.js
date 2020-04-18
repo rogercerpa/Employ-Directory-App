3e6f0f8110878c88d71e513ee4b3a1c3957ad567
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-18 00:55:09 -0400

    updated the API information

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 export default {
 	getRandomEmployee : function() {
-		return axios.get('');
+		return axios
+			.get('https://api.github.com/orgs/github/public_members')
+			.then((res) => {
+				const employees = res.data;
+				return employees.map((employee) => {
+					return {
+						login      : employee.login,
+						image      : employees.avatar_url,
+						profileUrl : employee.html_url
+					};
+				});
+			});
 	}
 };
