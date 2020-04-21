75c00fba86892d9bfb510e6b310e4c2c3e202c1f
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-20 22:19:25 -0400

    update the API function

--- src/components/Employees/index.js
+++ src/components/Employees/index.js
@@ -1,45 +1,66 @@
-import React from 'react';
-// import API from '../../utils/API';
+import React, { Component } from 'react';
+import API from '../../utils/API';
 
-function Employees() {
-	return (
-		<div>
-			<table class="table">
-				<thead class="thead-dark">
-					<tr>
-						<th scope="col">Image</th>
-						<th scope="col">Name</th>
-						<th scope="col">Phone</th>
-						<th scope="col">Email</th>
-						<th scope="col">DOB</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th scope="row">1</th>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>00/00/0000</td>
-					</tr>
-					<tr>
-						<th scope="row">2</th>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>00/00/0000</td>
-					</tr>
-					<tr>
-						<th scope="row">3</th>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>00/00/0000</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	);
+// employeeList = query =>{
+// 	API.search(query)
+
+// };
+
+class Employees extends Component {
+	state = {
+		result : {},
+		search : ''
+	};
+	componentDidMount() {
+		this.employeeList();
+	}
+
+	employeeList = (query) => {
+		API.getRandomEmployee(query)
+			.then((res) => this.setState({ result: res.data }, console.log(query)))
+			.catch((err) => console.log(err));
+	};
+
+	render() {
+		return (
+			<div>
+				<table className="table">
+					<thead className="thead-dark">
+						<tr>
+							<th scope="col">Image</th>
+							<th scope="col">Name</th>
+							<th scope="col">Phone</th>
+							<th scope="col">Email</th>
+							<th scope="col">DOB</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">1</th>
+							<td>Mark</td>
+							<td>Otto</td>
+							<td>@mdo</td>
+							<td>00/00/0000</td>
+						</tr>
+						<tr>
+							<th scope="row">2</th>
+							<td>Jacob</td>
+							<td>Thornton</td>
+							<td>@fat</td>
+							<td>00/00/0000</td>
+						</tr>
+						<tr>
+							<th scope="row">3</th>
+							<td>Larry</td>
+							<td>the Bird</td>
+							<td>@twitter</td>
+							<td>00/00/0000</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
 
 export default Employees;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,10 +4,10 @@ import './style.css';
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
 	return (
-		<div class="jumbotron jumbotron-fluid">
-			<div class="container">
-				<h1 class="display-4">Welcome to your Employee Directory</h1>
-				<p class="lead">
+		<div className="jumbotron jumbotron-fluid">
+			<div className="container">
+				<h1 className="display-4">Welcome to your Employee Directory</h1>
+				<p className="lead">
 					Click on carrot to filter by heading or use the search box to narrow
 					your results.
 				</p>
